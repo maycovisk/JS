88658e770f5c6d45e2f8ee173b9660b3c8951d64
Maycovisk <90663036+maycovisk@users.noreply.github.com>
2023-01-17 16:18:49 -0300

    Add files via upload

---
+++ Calculadora IMC/calculadoraimc.js
@@ -0,0 +1,10 @@
+const nome = document.getElementById('nome');
+const altura = document.getElementById('altura');
+const peso = document.getElementById('peso');
+const calcular = document.getElementById('calcular');
+const resultado = document.getElementById('resultado');
+
+function imc(){
+    
+}
+calcular.addEventListener('click', imc);
