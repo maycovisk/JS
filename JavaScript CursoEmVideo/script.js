24e93879fed5d6804a47c8cdb1229761c830cc4b
Maycovisk <90663036+maycovisk@users.noreply.github.com>
2023-02-03 08:48:27 -0300

    Add files via upload

---
+++ JavaScript CursoEmVideo/script.js
@@ -0,0 +1,9 @@
+var s = 'JavaScript'
+
+console.log('Eu estou apresendo s'); //não faz iterpolação
+console.log('Eu estou aprendendo '+ s); //usa concatenação
+console.log(`Eu estou apredendo $(s)`); //usar template string
+
+var n1 = 1234.5
+
+n1.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'}) //formatar valor monetário
